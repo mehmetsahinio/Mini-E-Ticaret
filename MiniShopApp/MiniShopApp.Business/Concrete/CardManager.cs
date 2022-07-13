64bf43539d480892d16f5ca5b9784ea071347435
mehmetsahinio <io.mehmetsahinn@gmail.com>
2022-07-13 21:16:38 +0300

    End

---
+++ MiniShopApp/MiniShopApp.Business/Concrete/CardManager.cs
@@ -0,0 +1,70 @@
+﻿using MiniShopApp.Business.Abstract;
+using MiniShopApp.Data.Abstract;
+using MiniShopApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.Business.Concrete
+{
+    public class CardManager : ICardService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CardManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void DeleteFromCard(string userId, int productId)
+        {
+            var card = GetCardByUserId(userId);
+            if (card!=null)
+            {
+                _unitOfWork.Cards.DeleteFromCard(card.Id,productId);
+                
+            }
+        }
+        public void AddToCard(string userId, int productId, int quantity)
+        {
+            var card = GetCardByUserId(userId);
+            if (card!=null)
+            {
+                var index = card.CardItems.FindIndex(i => i.ProductId == productId);
+                if (index<0)//eğer ürün daha card'da yoksa
+                {
+                    card.CardItems.Add(new CardItem()
+                    {
+                        ProductId=productId,
+                        Quantity=quantity,
+                        CardId=card.Id
+                    });
+                }
+                else
+                {
+                    card.CardItems[index].Quantity += quantity;
+                }
+                _unitOfWork.Cards.Update(card);
+                _unitOfWork.Save();
+            }
+        }
+
+        public Card GetCardByUserId(string userId)
+        {
+            return _unitOfWork.Cards.GetByUserId(userId);
+        }
+
+        public void InitializeCard(string userId)
+        {
+            _unitOfWork.Cards.Create(new Card() { UserId=userId});
+            _unitOfWork.Save();
+        }
+
+        public void ClearCard(int cardId)
+        {
+            _unitOfWork.Cards.ClearCard(cardId);
+        }
+    }
+}
---
+++ MiniShopApp/MiniShopApp.Data/Concrete/EfCore/EfCoreCardRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniShopApp.Data.Abstract;
+using MiniShopApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.Data.Concrete.EfCore
+{
+    public class EfCoreCardRepository : EfCoreGenericRepository<Card>, ICardRepository
+    {
+        public EfCoreCardRepository(MiniShopContext context) : base(context)
+        {
+
+        }
+        private MiniShopContext MiniShopContext
+        {
+            get { return _context as MiniShopContext; }
+        }
+        public void ClearCard(int cardId)
+        {
+            var cmd = @"DELETE FROM CardItems WHERE CardId=@p0";
+            MiniShopContext.Database.ExecuteSqlRaw(cmd, cardId);
+        }
+
+        public void DeleteFromCard(int cardId, int productId)
+        {
+            var cmd = @"DELETE FROM CardItems WHERE CardId=@p0 AND ProductId=@p1";
+            MiniShopContext.Database.ExecuteSqlRaw(cmd, cardId, productId);
+        }
+
+        public Card GetByUserId(string userId)
+        {
+            return MiniShopContext.Cards
+                .Include(i => i.CardItems)
+                .ThenInclude(i => i.Product)
+                .FirstOrDefault(i => i.UserId == userId);
+        }
+        public override void Update(Card entity)
+        {
+            MiniShopContext.Cards.Update(entity);
+        }
+    }
+}
